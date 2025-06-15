f73802fd02312dcbcef469435d7a112f1212b198
WILLIAM JAMES <161729235+Mrhonest42@users.noreply.github.com>
2025-06-15 19:46:04 +0530

    Update reg_check.php

--- ACIC/reg_check.php
+++ ACIC/reg_check.php
@@ -1,3 +1,4 @@
+<!-- reg_check.php // login logic -->
 <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
