582cd7b129483825d641edf61b669e22f0a303b9
WILLIAM JAMES <161729235+Mrhonest42@users.noreply.github.com>
2025-06-17 15:43:39 +0530

    Update reg_check.php

--- ACIC/reg_check.php
+++ ACIC/reg_check.php
@@ -1,11 +1,12 @@
-<!-- reg_check.php // login logic -->
+<!-- reg_check.php -->
+<?php
+session_start();
+?>
 <script src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert-dev.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css">
 
 <?php
-session_start();
-
 $host = 'localhost';
 $db = 'college';
 $user = 'root';
@@ -19,41 +20,32 @@ if ($con->connect_error) {
 if (isset($_POST['sub_reg_check'])) {
     $mobile = $_POST['txt_src_mobile'];
     $email = $_POST['txt_src_emailid'];
-
-    $query = "SELECT * FROM students WHERE mobile_number = '$mobile'";
+    
+    $query = "SELECT * FROM students WHERE mobile_number = '$mobile' and email='$email'";
     $result = mysqli_query($con, $query);
 
     if (mysqli_num_rows($result) > 0) {
         $row = mysqli_fetch_assoc($result);
-        $registered_email = $row['email'];
-
-        if ($email === $registered_email) {
+           
             // Store necessary session data
             $_SESSION['category'] = $row['category'];
-            $_SESSION['txt_src_mobile'] = $row['mobile_number'];
+            $_SESSION['txt_src_mobile'] = $mobile;
             $_SESSION['username'] = $row['name'];
+
             //Changes made here
             header("Location: payment.php");
-            exit();
-        } else {
+            
             echo "<script>
-                setTimeout(function() {
-                    swal({
-                        title: 'Login Failed!',
-                        text: 'Email does not match our records.',
-                        type: 'error'
-                    }, function() {
-                        window.location = 'index.php';
-                    });
-                }, 100);
+                document.querySelector('input[name=\"txt_src_mobile\"]').value = '';
+                document.querySelector('input[name=\"txt_src_emailid\"]').value = '';
             </script>";
-        }
+            exit();
     } else {
         echo "<script>
             setTimeout(function() {
                 swal({
                     title: 'Not Found!',
-                    text: 'No user found with this mobile number.',
+                    text: 'mobile number & Email id not matched',
                     type: 'error'
                 }, function() {
                     window.location = 'index.php';
